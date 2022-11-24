7e4f66ebf1af7f990b60e8df9549ef3e8129fa2a
SkyNett81 <kflaathe@gmail.com>
2022-11-24 18:35:47 +0100

    Update ubuntu-dist-upgrade.sh

--- ubuntu-dist-upgrade.sh
+++ ubuntu-dist-upgrade.sh
@@ -8,16 +8,55 @@ echo "$greeting back $user! Today is $day, which is the best day of the entire w
 echo "Your Bash shell version is: $BASH_VERSION. Enjoy!"
 
 
+TEXT_RESET='\e[0m'
+TEXT_YELLOW='\e[0;33m'
+TEXT_RED_B='\e[1;31m'
 
-sudo apt update 
+sudo apt-get update -y
+echo -e $TEXT_YELLOW
+echo 'APT update finished...'
+echo -e $TEXT_RESET
 
-sudo apt upgrade
+sudo apt-get upgrade -y
+echo -e $TEXT_YELLOW
+echo 'APT upgrade finished...'
+echo -e $TEXT_RESET
 
-sudo apt dist-upgrade
+sudo apt dist-upgrade -y
+echo -e $TEXT_YELLOW
+echo 'APT distributive upgrade finished...'
+echo -e $TEXT_RESET
 
-sudo apt autoremove
+sudo apt-get autoremove -y
+echo -e $TEXT_YELLOW
+echo 'APT auto remove finished...'
+echo -e $TEXT_RESET
 
-sudo apt install update-manager-core
+sudo sudo apt install update-manager-core -y
+echo -e $TEXT_YELLOW
+echo 'APT auto install update-manager finished...'
+echo -e $TEXT_RESET
 
-sudo do-release-upgrade
+sudo do-release-upgrade 
+echo -e $TEXT_YELLOW
+echo 'APT release upgrade finished...'
+echo -e $TEXT_RESET
 
+if [ -f /var/run/reboot-required ]; then
+    echo -e $TEXT_RED_B
+    echo 'Reboot required!'
+    echo -e $TEXT_RESET
+fi
+
+echo -e $TEXT_RED_B
+echo "Update Complete! Press Y/N to reboot."
+echo -e $TEXT_RESET
+
+while true; do
+    read -p "Would you like to reboot now? " yn
+    case $yn in
+	[Yy]* ) reboot; break;;
+	[Nn]* ) exit;;
+	* ) echo "Please answer yes or no!";;
+    esac
+done
