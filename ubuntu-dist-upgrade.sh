2e22f84c21e330daf3d69d0b96f77114d840fb04
SkyNett81 <kflaathe@gmail.com>
2022-11-24 18:13:12 +0100

    Update ubuntu-dist-upgrade.sh

--- ubuntu-dist-upgrade.sh
+++ ubuntu-dist-upgrade.sh
@@ -1,3 +1,14 @@
+#!/bin/bash
+
+greeting="Welcome"
+user=$(whoami)
+day=$(date +%A)
+
+echo "$greeting back $user! Today is $day, which is the best day of the entire week!"
+echo "Your Bash shell version is: $BASH_VERSION. Enjoy!"
+
+
+
 sudo apt update 
 
 sudo apt upgrade
